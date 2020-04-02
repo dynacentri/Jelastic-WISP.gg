93316b46dc0dcf803c91c1a74b5249180c7871ce
William Gill <william.gill@anomalous.dev>
2020-04-02 13:51:20 -0500

    Still learning

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 eval 'apt install -y --no-install-recommends --no-install-suggests wget curl'
 eval 'curl -sSL https://get.docker.com/ | sh'
 eval 'systemctl enable docker'
 eval 'systemctl start docker'
+
 END
--- scripts/setup_wisp.sh
+++ scripts/setup_wisp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 LICENSE="$1"
 DOMAIN="$2"
 
-eval 'bash <(wget -qO- "https://wisp.gg/install?license_key=${LICENSE}&instance_url=https%3A%2F%2F${DOMAIN}")'
+eval 'tmux new-session -d -s wisp_install'
+eval 'tmux send-keys "bash <(wget -qO- "https://wisp.gg/install?license_key=${LICENSE}&instance_url=https%3A%2F%2F${DOMAIN}")" C-m'
 
 END
