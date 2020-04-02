e4081d9aea0ad5761a8b4ccd27c22905603a0d4a
William Gill <william.gill@anomalous.dev>
2020-04-02 14:05:25 -0500

    Shell Scripts

--- scripts/setup_wisp.sh
+++ scripts/setup_wisp.sh
@@ -3,7 +3,6 @@
 LICENSE="$1"
 DOMAIN="$2"
 
-eval 'tmux new-session -d -s wisp_install'
-eval 'tmux send-keys "bash <(wget -qO- "https://wisp.gg/install?license_key=${LICENSE}&instance_url=https%3A%2F%2F${DOMAIN}")" C-m'
+eval 'bash <(wget -qO- "https://wisp.gg/install?license_key=${LICENSE}&instance_url=https%3A%2F%2F${DOMAIN}")'
 
 END
