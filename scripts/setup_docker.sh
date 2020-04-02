d1f52ab0ec8ba05931d2fb552301b264c6bfdc8c
William Gill <william.gill@anomalous.dev>
2020-04-02 14:51:18 -0500

    The JPS Manifest

--- scripts/setup_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-eval 'apt install -y --no-install-recommends --no-install-suggests tmux curl'
-eval 'curl -sSL https://get.docker.com/ | sh'
-eval 'systemctl enable docker'
-eval 'systemctl start docker'
-
-END
--- scripts/setup_wisp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LICENSE="$1"
-DOMAIN="$2"
-
-eval 'bash <(wget -qO- "https://wisp.gg/install?license_key=${LICENSE}&instance_url=https%3A%2F%2F${DOMAIN}")'
-
-END
